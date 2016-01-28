3c69decd70cf922742b68aa8468ee7a6f27f6ca6
Howard <Howard@HowarddeAir.gateway>
2016-01-28 17:36:49 +1100

    Remove database.yml.example and secrets.yml.example

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,16 +3,18 @@ lock '3.4.0'
 
 set :application, 'agriculture_webapp'
 # set :repo_url, 'git@example.com:me/my_repo.git' #COMMENT for experience deployng without github
-set :repo_url, 'file://~/Code/agriculture_webapp.git'
+set :repo_url, 'file://Users/Howard/Code/agriculture_webapp/agriculture_webapp.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/agriculture_webapp'
 
 # Default value for :scm is :git
 # set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -25,9 +27,11 @@ set :repo_url, 'file://~/Code/agriculture_webapp.git'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
