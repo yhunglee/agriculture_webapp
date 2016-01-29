f56841a37a78405c3b1b5fadcf07949dbad1d0fe
Howardsun <howard@csie.io>
2016-01-29 17:43:18 +1100

    Adjust base.rb to another mounting directives.

--- app/controllers/api/V1/overview_vegetables.rb
+++ app/controllers/api/V1/overview_vegetables.rb
@@ -1,42 +1,38 @@
 require 'doorkeeper/grape/helpers'
-module API
-       module V1	
-		class OverviewVegetables < Grape::API
-			format :json
-			default_format :json
+class OverviewVegetables < Grape::API
+	format :json
+	default_format :json
 
-		        version 'v1', using: :path
+	version 'v1', using: :path
 
-			helpers Doorkeeper::Grape::Helpers
-			include Grape::Kaminari
+	helpers Doorkeeper::Grape::Helpers
+	include Grape::Kaminari
 
-			before do 
-				doorkeeper_authorize!
+	before do 
+		doorkeeper_authorize!
+	end 
+
+	resource :overview_vegetables do 
+		desc "Get all transaction prices of all items today."
+		paginate per_page: 50, max_per_page: 100, offset: 0
+		get :today do
+			OverviewVegetable.where(transaction_date: Date.today).find_in_batches.order(:name) do |vegetables|
+				paginate(vegetables)
 			end 
+		end
 
-			resource :overview_vegetables do 
-				desc "Get all transaction prices of all items today."
-				paginate per_page: 50, max_per_page: 100, offset: 0
-				get :today do
-					OverviewVegetable.where(transaction_date: Date.today).find_in_batches.order(:name) do |vegetables|
-						paginate(vegetables)
-					end 
-				end
-
-				desc "Get transaction prices of delegated item in a delegated day."
-				params do
-					requires :transaction_date, type: Date, desc: 'code'
-					optional :name, type: String, allow_blank: false
-					optional :item_code, type: String, allow_blank: false 
-				end 
-				get '/' do
-					conditions = Hash[{date: params[:transaction_date], name: params[:name], code: params[:item_code]}.select{|k,v| v.present?}]
-					OverviewVegetable.where(conditions) do |vegetables|
-						paginate(vegetables.order(:name, :date))
-					end 
-				end 
-
-			end
+		desc "Get transaction prices of delegated item in a delegated day."
+		params do
+			requires :transaction_date, type: Date, desc: 'code'
+			optional :name, type: String, allow_blank: false
+			optional :item_code, type: String, allow_blank: false 
 		end 
-       end
-end
+		get '/' do
+			conditions = Hash[{date: params[:transaction_date], name: params[:name], code: params[:item_code]}.select{|k,v| v.present?}]
+			OverviewVegetable.where(conditions) do |vegetables|
+				paginate(vegetables.order(:name, :date))
+			end 
+		end 
+
+	end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users 
 
-  mount API::Base => '/api'
+  mount Base => '/api'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
