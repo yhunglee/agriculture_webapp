c7f10286214ba1fc15d92c751e36b2817d596459
Howardsun <howard@csie.io>
2016-01-29 18:32:49 +1100

    Fix syntax error in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module AgricultureWebapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.paths.add File.join('app', 'controllers', 'api'), glob: File.join('**','**', '*.rb')
-    config.autoload_paths += Dir[Rails.root.join('app','controllers','api', '**', '**', '*']
+    config.autoload_paths += Dir[Rails.root.join('app','controllers','api', '**', '**', '*')]
   end
 end
