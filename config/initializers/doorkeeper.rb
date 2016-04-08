7d8f10892150dfd855aab62bbbb23e39b585d911
Howard <yhunglee@users.noreply.github.com>
2016-04-08 21:27:02 +0800

    Add admin column to user table for restricting to access pages of oauth applications of doorkeeper.

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -14,13 +14,16 @@ Doorkeeper.configure do
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
+  admin_authenticator do
   #   # Put your admin authentication logic here.
   #   # Example implementation:
   #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+	  if !(user_signed_in? && current_user.admin?)
+		  flash.now[:warning] = "You are not an admin."
+	  	  redirect_to root_url
+	  end 
+  end
   #
-  # 20160219: Need to configure.
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323090312) do
+ActiveRecord::Schema.define(version: 20160408115833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,22 +109,23 @@ ActiveRecord::Schema.define(version: 20160323090312) do
   add_index "specified_vegetable", ["trade_location"], name: "index_specified_vegetable_on_trade_location", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "admin",                              default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
